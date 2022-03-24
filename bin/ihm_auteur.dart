e2c82f3e342cea7d3cb0491a2b1cfb656b7a2118
francois <admin@purpleguy.ovh>
2022-03-24 13:10:54 +0100

    création du menu auteur et editeur

---
+++ bin/ihm_auteur.dart
@@ -0,0 +1,15 @@
+class IHMAuteur {
+  static void affichemenu() {
+    print("--------------------------------");
+    print("menu gestion de la table Auteur");
+    print("");
+    print("+------------------------+");
+    print("| 1 - consulter la table |");
+    print("| 2 - modifié une valeur |");
+    print("| 3 - inséré une valeur  |");
+    print("| 4 - suprimé une valeur |");
+    print("|                        |");
+    print("| 0 - quitter            |");
+    print("+------------------------+");
+  }
+}
---
+++ bin/imh_editeur.dart
@@ -0,0 +1,15 @@
+class IHMEditeur {
+  static void affichemenu() {
+    print("--------------------------------");
+    print("menu gestion de la table editeur");
+    print("");
+    print("+------------------------+");
+    print("| 1 - consulter la table |");
+    print("| 2 - modifié une valeur |");
+    print("| 3 - inséré une valeur  |");
+    print("| 4 - suprimé une valeur |");
+    print("|                        |");
+    print("| 0 - quitter            |");
+    print("+------------------------+");
+  }
+}
