91716b71591a3e54add5da1b828a1352671be87c
francois <admin@purpleguy.ovh>
2022-03-24 15:12:56 +0100

    modification interface produit / editeur / auteur

--- bin/ihm_principale.dart
+++ bin/ihm_principale.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+import 'ihm_auteur.dart';
+import 'ihm_produit.dart';
+import 'imh_editeur.dart';
+
 class IHMprincipale {
   static void titre() {
     print("");
@@ -7,7 +11,7 @@ class IHMprincipale {
     print("le furet du nord");
     print("--------------------------------------------------");
   }
-  
+
   static void quitter() {
     print("");
     print("--------------------------------------------------");
@@ -24,21 +28,20 @@ class IHMprincipale {
     int choix = -1;
     while (choix != 0) {
       print("Menu Principal");
-      print("1- Gestion de la BDD");
-      print("2- Gestion de la table Produit");
-      print("3- Gestion de la table Editeur");
-      print("4- Gestion de la table Auteur");
+      print("1- Gestion de la table Produit");
+      print("2- Gestion de la table Editeur");
+      print("3- Gestion de la table Auteur");
       print("0- Quitter");
       choix = IHMprincipale.choixMenu(3);
       print("--------------------------------------------------");
       if (choix == 1) {
         await IHMprincipale.menuBDD();
       } else if (choix == 2) {
-        await IHMProduit.menu();
+        await IHMProduit.affichemenu();
       } else if (choix == 3) {
-        await IHMEditeur.menu();
-      }else if (choix == 4) {
-        await IHMAuteur.menu();
+        await IHMEditeur.affichemenu();
+      } else if (choix == 4) {
+        await IHMAuteur.affichemenu();
       }
     }
     return 0;
