7f8f026f4baf6b8bc0bca2cb7b6567d15e230b61
Lucaslala88 <delfossesomain@gmail.com>
2022-03-24 11:56:54 +0100

    debut du menu lucas

--- bin/ihm_principale.dart
+++ bin/ihm_principale.dart
@@ -7,14 +7,40 @@ class IHMprincipale {
     print("le furet du nord");
     print("--------------------------------------------------");
   }
-
+  
   static void quitter() {
     print("");
     print("--------------------------------------------------");
     print("au revoir");
     print("programme dévelopé par");
     print("François Oudart");
-    print("lucas Delfosse");
+    print("Lucas Delfosse");
     print("--------------------------------------------------");
+  }
+
+  // methode des menus et actions
+  // menu d'accueil
+  static Future<int> menu() async {
+    int choix = -1;
+    while (choix != 0) {
+      print("Menu Principal");
+      print("1- Gestion de la BDD");
+      print("2- Gestion de la table Produit");
+      print("3- Gestion de la table Editeur");
+      print("4- Gestion de la table Auteur");
+      print("0- Quitter");
+      choix = IHMprincipale.choixMenu(3);
+      print("--------------------------------------------------");
+      if (choix == 1) {
+        await IHMprincipale.menuBDD();
+      } else if (choix == 2) {
+        await IHMProduit.menu();
+      } else if (choix == 3) {
+        await IHMEditeur.menu();
+      }else if (choix == 4) {
+        await IHMAuteur.menu();
+      }
+    }
+    return 0;
   }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -2,5 +2,6 @@ import 'ihm_principale.dart';
 
 void main(List<String> arguments) async {
   IHMprincipale.titre();
+  await IHMprincipale.menu();
   IHMprincipale.quitter();
 }
