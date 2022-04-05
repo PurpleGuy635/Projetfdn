c4f5f673c2fdb8bc64cb844ff445e872dba02cfb
Lucaslala88 <delfossesomain@gmail.com>
2022-04-05 14:42:36 +0200

    gestion bdd lucas

--- bin/ihm_principale.dart
+++ bin/ihm_principale.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-// import 'package:mysql1/mysql1.dart';
+import 'package:mysql1/mysql1.dart';
 
 import 'ihm_auteur.dart';
 import 'ihm_produit.dart';
@@ -45,7 +45,7 @@ class IHMprincipale {
     return i;
   }
 
-   // retourne un boolean pour demande de confirmation
+   // module de confirmation
   static bool confirmation() {
     bool saisieValide = false;
     bool confirme = false;
@@ -72,10 +72,10 @@ class IHMprincipale {
     while (!saisieValide) {
       print("> Veuillez saisir le mot de passe :");
       try {
-        stdin.echoMode = false;
+        stdin.echoMode = false; //permet de mettre un mdp en invisible
         s = stdin.readLineSync().toString();
         saisieValide = true;
-        stdin.echoMode = true;
+        stdin.echoMode = true;//permet de remettre la visibiliter
       } catch (e) {
         print("Erreur dans la saisie.");
       }
@@ -99,7 +99,6 @@ class IHMprincipale {
     return s;
   }
 
-  // retourne un int pour saisie d'entier positif
   static int saisieInt() {
     bool saisieValide = false;
     int i = -1;
@@ -115,7 +114,7 @@ class IHMprincipale {
     return i;
   }
 
-    // retourne un int pour saisie d'entier positif
+    
   static int saisieID() {
     bool saisieValide = false;
     int i = -1;
@@ -135,21 +134,21 @@ class IHMprincipale {
     return i;
   }
 
- // methode des menus et actions
-  // menu setting
-  // static ConnectionSettings setting() {
-  //   String bdd = IHMprincipale.saisieString("le nom de la BDD");
-  //   String user = IHMprincipale.saisieString("l'utilisateur");
-  //   String mdp = IHMprincipale.saisieMDP();
+ //methode des menus et actions
+ //menu setting
+  static ConnectionSettings setting() {
+    String bdd = IHMprincipale.saisieString("le nom de la BDD");
+    String user = IHMprincipale.saisieString("l'utilisateur");
+    String mdp = IHMprincipale.saisieMDP();
 
-  //   return ConnectionSettings(
-  //     host: 'localhost',
-  //     port: 3306,
-  //     user: user, // DartUser
-  //     password: mdp, // dartmdp
-  //     db: bdd, // DartDB
-  //   );
-  // }
+    return ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: user,
+      password: mdp,
+      db: bdd,
+    );
+  }
 
   // methode des menus et actions
   // menu d'accueil
@@ -176,4 +175,106 @@ class IHMprincipale {
     }
     return 0;
   }
+  // menu de la bdd
+  static Future<void> menuBDD(ConnectionSettings settings) async {
+    int choix = -1;
+    while (choix != 0) {
+      print("Menu - Gestion BDD");
+      print("1- Création des tables de la BDD");
+      print("2- Verification des tables de la BDD");
+      print("3- Afficher les tables de la BDD");
+      print("4- Supprimer une table dans la BDD");
+      print("5- Supprimer toutes les tables dans la BDD");
+      print("0- Quitter");
+      choix = IHMprincipale.choixMenu(5);
+      print("--------------------------------------------------");
+
+      if (choix == 1) {
+        await IHMprincipale.createTable(settings);
+      } else if (choix == 2) {
+        await IHMprincipale.checkTable(settings);
+      } else if (choix == 3) {
+        await IHMprincipale.selectTable(settings);
+      } else if (choix == 4) {
+        await IHMprincipale.deleteTable(settings);
+      } else if (choix == 5) {
+        await IHMprincipale.deleteAllTables(settings);
+      }
+    }
+    print("Retour menu précédent.");
+    print("--------------------------------------------------");
+    await Future.delayed(Duration(seconds: 1));
+  }
+
+  // permet de creer les tables
+  static Future<void> createTable(ConnectionSettings settings) async {
+    print("Création des tables manquantes dans la BDD ...");
+    await DBConfig.createTables(settings);
+    print("Fin de l'opération.");
+    print("--------------------------------------------------");
+    IHMprincipale.wait();
+  }
+
+// verifie les tables
+  static Future<void> checkTable(ConnectionSettings settings) async {
+    print("Verification des tables dans la BDD ...");
+    if (await DBConfig.checkTables(settings)) {
+      print("Toutes les tables sont présentes dans la BDD.");
+    } else {
+      print("Il manque des tables dans la BDD.");
+    }
+    print("Fin de l'opération.");
+    print("--------------------------------------------------");
+    IHMprincipale.wait();
+  }
+
+// permet d'afficher une table
+  static Future<void> selectTable(ConnectionSettings settings) async {
+    List<String> listTable = await DBConfig.selectTables(settings);
+    print("Liste des tables :");
+    for (var table in listTable) {
+      print("- $table");
+    }
+    print("Fin de l'opération.");
+    print("--------------------------------------------------");
+    IHMprincipale.wait();
+  }
+
+// permet de supprimer une table
+  static Future<void> deleteTable(ConnectionSettings settings) async {
+    print("Quelle table voulez vous supprimer ?");
+    String table = IHMprincipale.saisieString("le nom de la table");
+    if (IHMprincipale.confirmation()) {
+      DBConfig.dropTable(settings, table);
+      print("Table supprimée.");
+      print("Fin de l'opération.");
+      print("--------------------------------------------------");
+      IHMprincipale.wait();
+    } else {
+      print("Annulation de l'opération.");
+      print("--------------------------------------------------");
+      IHMprincipale.wait();
+    }
+  }
+
+// action pour supprimer les tables
+  static Future<void> deleteAllTables(ConnectionSettings settings) async {
+    if (IHMprincipale.confirmation()) {
+      DBConfig.dropAllTable(settings);
+      print("Tables supprimées.");
+      print("Fin de l'opération.");
+      print("--------------------------------------------------");
+      IHMprincipale.wait();
+    } else {
+      print("Annulation de l'opération.");
+      print("--------------------------------------------------");
+      IHMprincipale.wait();
+    }
+  }
+
+  static void wait() {}
+}
+
+class ConnectionSettings {
+}
 }
